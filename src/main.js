1dd8e4a1c5d111db506bc0e67bf26a6d66dc1bc5
Tiago França <tiago@tiagofranca.com>
2024-08-16 23:05:21 -0300

    alpine logic
    	modified:   index.html
    	modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,31 @@ window.Alpine = Alpine;
 document.addEventListener('alpine:init', () => {
     Alpine.data('todoData', () => ({
         todos: [1, 32, 4, ],
+        newTodo: false,
+        editTodo: null,
+        get showModal() {
+            return this.newTodo || this.editTodo;
+        },
+
+        editItem(id) { // WIP
+            this.newTodo = false,
+            this.editTodo = {
+                id,
+                title: 'Titulo',
+                category: 'todo',
+                description: `algo aqui\npara testar`,
+            };
+        },
+
+        deleteItem(id) { // TODO
+            this.newTodo = false,
+            this.editTodo = null;
+        },
+
+        dismissModal() {
+            this.newTodo = false;
+            this.editTodo = null;
+        },
 
         toggleDone(todo) {
             todo['done'] = !todo['done'];
